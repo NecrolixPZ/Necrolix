e05aa2345628d838d8d416ce8d71dd9689136510
Brov3r <147500212+Brov3r@users.noreply.github.com>
2024-10-18 13:17:13 +0300

    Update launcher

--- src/main/java/org/necrolix/Main.java
+++ src/main/java/org/necrolix/Main.java
@@ -1,11 +1,9 @@
 package org.necrolix;
 
 import java.io.File;
+import java.lang.management.ManagementFactory;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Main class for running the Project Zomboid server as a separate application.
@@ -16,6 +14,7 @@ public class Main {
 
     /**
      * The main method that starts the application.
+     * NOTE: This implementation of running the server is test, in the future it may change to a more productive one.
      *
      * @param args command line arguments to pass to the process.
      * @throws Exception if a runtime error occurs.
@@ -23,7 +22,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         String jdkVersion = System.getProperty("java.version");
         String osName = System.getProperty("os.name");
-        boolean isAmd64 = System.getProperty("os.arch").equalsIgnoreCase("amd64");
+        String osArch = System.getProperty("os.arch");
+        boolean is64bit = osArch.contains("64");
 
         File coreFile = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI());
 
@@ -31,7 +31,7 @@ public class Main {
         checkJDK(jdkVersion);
 
         // Checking the bit and adding native libraries
-        addNatives(coreFile, osName, isAmd64);
+        addNatives(coreFile, osName, is64bit);
 
         // Adding to classpath
         addClassPaths(coreFile);
@@ -39,24 +39,48 @@ public class Main {
         // Starting a new process with all settings
         List<String> command = new ArrayList<>();
         command.add(System.getProperty("java.home") + File.separator + "bin" + File.separator + "java");
+        command.addAll(ManagementFactory.getRuntimeMXBean().getInputArguments());
         command.add("-Djava.awt.headless=true");
         command.add("-Dzomboid.steam=1");
         command.add("-Dzomboid.znetlog=1");
-        command.add(isAmd64 ? "-XX:+UseZGC" : "-XX:+UseG1GC");
+        command.add(is64bit ? "-XX:+UseZGC" : "-XX:+UseG1GC");
         command.add("-Djava.library.path=" + String.join(";", nativePaths));
         command.add("-cp");
         command.add(String.join(";", classPaths));
         command.add(Launcher.class.getName());
         Collections.addAll(command, args);
 
-        System.out.printf("[#] Starting the server | OS: %s | Arch: %s | JDK: %s | Core: %s%n", osName, isAmd64 ? "x64" : "x32", jdkVersion, coreFile.getName().replace(".jar", ""));
+        System.out.printf("[#] Starting the server | OS: %s | Arch: %s | JDK: %s | Core: %s%n", osName, is64bit ? "x64" : "x32", jdkVersion, coreFile.getName().replace(".jar", ""));
 
         ProcessBuilder processBuilder = new ProcessBuilder(command);
+        addPreloadEnv(osName, processBuilder, coreFile);
         processBuilder.inheritIO();
         Process process = processBuilder.start();
 
         int exitCode = process.waitFor();
         System.exit(exitCode);
+    }
+
+    /**
+     * Updates the environment of the provided {@link ProcessBuilder} to include the {@code LD_PRELOAD}
+     * environment variable, if the system is UNIX-based and the {@code libjsig.so} file exists in the
+     * parent directory of the provided {@code coreFile}.
+     *
+     * @param osName         the name of the operating system, typically obtained from {@code System.getProperty("os.name")}
+     * @param processBuilder the {@link ProcessBuilder} instance whose environment will be modified
+     * @param coreFile       the {@link File} representing the current JAR application, used to determine the path to {@code libjsig.so}
+     */
+    private static void addPreloadEnv(String osName, ProcessBuilder processBuilder, File coreFile) {
+        if (!osName.contains("linux")) return;
+
+        File jsigFile = coreFile.getParentFile().toPath().resolve("libjsig.so").toFile();
+
+        if (!jsigFile.exists()) return;
+
+        Map<String, String> environment = processBuilder.environment();
+        String ldPreload = environment.getOrDefault("LD_PRELOAD", "");
+        ldPreload += (ldPreload.isEmpty() ? "" : ":") + jsigFile.getAbsolutePath();
+        environment.put("LD_PRELOAD", ldPreload);
     }
 
     /**
@@ -74,23 +98,23 @@ public class Main {
         classPaths.add(javaFolder.getAbsolutePath());
 
         File[] files = javaFolder.listFiles();
-        if (files != null) {
-            classPaths.addAll(Arrays.stream(files)
-                    .filter(file -> file.isFile() && file.getName().endsWith(".jar"))
-                    .map(File::getAbsolutePath)
-                    .toList());
-        } else {
+        if (files == null) {
             System.out.println("[!] Unable to list files in 'java' directory.");
             System.exit(-1);
         }
+
+        classPaths.addAll(Arrays.stream(files)
+                .filter(file -> file.isFile() && file.getName().endsWith(".jar"))
+                .map(File::getAbsolutePath)
+                .toList());
     }
 
     /**
      * Adds native library paths to the nativePaths list.
      *
      * @param coreFile the application's core JAR file.
-     * @param osName name of the operating system.
-     * @param isAmd64 flag indicating whether the architecture is 64-bit.
+     * @param osName   name of the operating system.
+     * @param isAmd64  flag indicating whether the architecture is 64-bit.
      */
     private static void addNatives(File coreFile, String osName, boolean isAmd64) {
         nativePaths.add(coreFile.getParent());
